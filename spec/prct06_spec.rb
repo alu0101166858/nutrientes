ee0a5f7561af5c5c63ac04d477de91690222ed5b
Jan-Philipp Kemmer <alu0101166858@ull.edu.es>
2017-10-26 18:02:25 +0100

    some improvements

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -10,27 +10,31 @@ RSpec.describe Prct06 do
     expect(true).to eq(true)
   end
 
+it "can be initialized" do
+    expect(Alimentos.new("nombre", 1, 2, 3).class).to eq(Alimentos)
+  end
+
   it "can be initialized and has a name" do
-    expect("nombre").to eq(Alimentos.new("nombre", 1, 1, 1).name)
+    expect(Alimentos.new("nombre", 1, 1, 1).name).to eq("nombre")
   end
 
-  it "Existe un m´etodo para obtener la cantidad de prote´ınas de un alimento" do
-    expect(10).to eq(Alimentos.new("nombre", 10, 1, 1).proteinas)
+  it "Existe un metodo para obtener la cantidad de proteınas de un alimento" do
+    expect(Alimentos.new("nombre", 10, 1, 1).proteinas).to eq(10)
   end
 
-  it "Existe un m´etodo para obtener la cantidad de gl´ucidos de un alimento" do
-    expect(10).to eq(Alimentos.new("nombre", 1, 10, 1).glucidos)
+  it "Existe un metodo para obtener la cantidad de glucidos de un alimento" do
+    expect(Alimentos.new("nombre", 1, 10, 1).glucidos).to eq(10)
   end
 
-  it "Existe un m´etodo para obtener de grasas de un alimento" do
-    expect(10).to eq(Alimentos.new("nombre", 1, 1, 10).lipidos)
+  it "Existe un metodo para obtener de grasas de un alimento" do
+    expect(Alimentos.new("nombre", 1, 1, 10).lipidos).to eq(10)
   end
 
-  it "Existe un m´etodo para obtener el alimento formateado" do
-    expect("(Alimento: Huevo frito, proteinas: 14.1, glucidos: 0.0, lipidos: 19.5)").to eq(Alimentos.new("Huevo frito", 14.1, 0.0, 19.5).format)
+  it "Existe un metodo para obtener el alimento formateado" do
+    expect(Alimentos.new("Huevo frito", 14.1, 0.0, 19.5).format).to eq("(Alimento: Huevo frito, proteinas: 14.1, glucidos: 0.0, lipidos: 19.5)")
   end
 
-  it "Existe un m´etodo para obtener el valor energ´etico de un alimento" do
-    expect(231.9).to eq(Alimentos.new("Huevo frito", 14.1, 0.0, 19.5).energy)
+  it "Existe un metodo para obtener el valor energetico de un alimento" do
+    expect(Alimentos.new("Huevo frito", 14.1, 0.0, 19.5).energy).to eq(231.9)
   end
 end
