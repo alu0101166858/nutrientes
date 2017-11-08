4057aeb78faf0ce30e69074af7629d6cbcef6bca
Jan-Philipp Kemmer <alu0101166858@ull.edu.es>
2017-11-08 11:49:30 +0000

    second task RUNNING

--- lib/prct06/alimentos.rb
+++ lib/prct06/alimentos.rb
@@ -25,27 +25,21 @@ class Alimentos
   end
 end
 
+#jerarquía de clases que permita clasificar los alimentos por grupos
 class HuevosLacteosHelados < Alimentos
 end
-
 class CarnesDerivados < Alimentos
 end
-
 class PescadosMariscos < Alimentos
 end
-
 class AlimentosGrasos < Alimentos
 end
-
 class AlimentosCarbohidratos < Alimentos
 end
-
 class VerdurasHortalizas < Alimentos
 end
-
 class Frutas < Alimentos
 end
-
 class Bebidas < Alimentos
 end
 
